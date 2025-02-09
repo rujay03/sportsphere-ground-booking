45cddec5e8f2b75f6247d2a0b8dd9948db3944e4
Ruwanthi Jayasinghe <ruwanthi.jayasinghe2002@gmail.com>
2025-02-09 06:39:16 +0530

    init commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sportsphere2;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+    
+    String[] a1 = new String[100];
+    String[] a2 = new String[100];
+    String[] a3 = new String[100];
+    String[] a4 = new String[100];
+    String[] a5 = new String[100];
+    String[] a6 = new String[100];
+    String[] a7 = new String[100];
+    String[] a8 = new String[100];
+    String[] a9 = new String[100];
+    String[] a10 = new String[100];
+         
+    
+    String firstName;
+    String lastName;
+    String email;
+    String address;
+    String phone;
+    
+    
+    
+    String url="jdbc:mysql://localhost:3307/Sportsphere2";
+    String user = "root";
+    String pss="7227876";
+    
+    
+}
